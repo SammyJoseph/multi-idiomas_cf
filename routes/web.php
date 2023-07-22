6ae4c339f06f5627b6b2118262a67420b1a2d121
SammyJoseph <sam.tab.paz@gmail.com>
2023-07-22 12:06:06 -0500

    clase #6 finalizada

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/features', function () {
+    return view('features');
+})->name('features');
+
+Route::get('/classes', function () {
+    return view('classes');
+})->name('classes');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
