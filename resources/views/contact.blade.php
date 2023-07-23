34645a7fe6a1d5ded31a3078329847114d2ab483
Sammy Joseph <sam.tab.paz@gmail.com>
2023-07-23 17:01:43 -0500

    clases #14 a #17 finalizadas

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,11 +6,11 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header mb-5">
         <div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style="min-height: 400px">
-            <h4 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase font-weight-bold">Contact Us</h4>
+            <h4 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase font-weight-bold">{{ __('Contact Us') }}</h4>
             <div class="d-inline-flex">
-                <p class="m-0 text-white"><a class="text-white" href="">Home</a></p>
+                <p class="m-0 text-white"><a class="text-white" href="">{{ __('Home') }}</a></p>
                 <p class="m-0 text-white px-2">/</p>
-                <p class="m-0 text-white">Contact Us</p>
+                <p class="m-0 text-primary">{{ __('Contact Us') }}</p>
             </div>
         </div>
     </div>
@@ -20,23 +20,23 @@
     <!-- Contact Start -->
     <div class="container pt-5">
         <div class="d-flex flex-column text-center mb-5">
-            <h4 class="text-primary font-weight-bold">Get In Touch</h4>
-            <h4 class="display-4 font-weight-bold">Email Us For Any Query</h4>
+            <h4 class="text-primary font-weight-bold">{{ __('Get In Touch') }}</h4>
+            <h4 class="display-4 font-weight-bold">{{ __('Email Us For Any Query') }}</h4>
         </div>
         <div class="row px-3 pb-2">
             <div class="col-sm-4 text-center mb-3">
                 <i class="fa fa-2x fa-map-marker-alt mb-3 text-primary"></i>
-                <h4 class="font-weight-bold">Address</h4>
+                <h4 class="font-weight-bold">{{ __('Address') }}</h4>
                 <p>123 Street, New York, USA</p>
             </div>
             <div class="col-sm-4 text-center mb-3">
                 <i class="fa fa-2x fa-phone-alt mb-3 text-primary"></i>
-                <h4 class="font-weight-bold">Phone</h4>
+                <h4 class="font-weight-bold">{{ __('Phone') }}</h4>
                 <p>+012 345 6789</p>
             </div>
             <div class="col-sm-4 text-center mb-3">
                 <i class="far fa-2x fa-envelope mb-3 text-primary"></i>
-                <h4 class="font-weight-bold">Email</h4>
+                <h4 class="font-weight-bold">{{ __('Email') }}</h4>
                 <p>info@example.com</p>
             </div>
         </div>
@@ -49,23 +49,23 @@
                     <div id="success"></div>
                     <form name="sentMessage" id="contactForm" novalidate="novalidate">
                         <div class="control-group">
-                            <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                            <input type="text" class="form-control" id="name" placeholder="{{ __('Your Name') }}" required="required" data-validation-required-message="Please enter your name" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                            <input type="email" class="form-control" id="email" placeholder="{{ __('Your Email') }}" required="required" data-validation-required-message="Please enter your email" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <input type="text" class="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                            <input type="text" class="form-control" id="subject" placeholder="{{ __('Subject') }}" required="required" data-validation-required-message="Please enter a subject" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <textarea class="form-control" rows="6" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                            <textarea class="form-control" rows="6" id="message" placeholder="{{ __('Message') }}" required="required" data-validation-required-message="Please enter your message"></textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                         <div>
-                            <button class="btn btn-outline-primary" type="submit" id="sendMessageButton">Send Message</button>
+                            <button class="btn btn-outline-primary" type="submit" id="sendMessageButton">{{ __('Send Message') }}</button>
                         </div>
                     </form>
                 </div>
